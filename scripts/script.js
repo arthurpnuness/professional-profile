42a286ab791f8d3786b15a13fe5167d0725f915a
Arthur Nunes <arthurpessoanunes@gmail.com>
2025-02-14 07:37:41 -0300

    commit

--- scripts/script.js
+++ scripts/script.js
@@ -1,22 +1,25 @@
 // Conteúdo do currículo em diferentes idiomas
-const curriculo = {
+const index = {
     pt: {
         titulo: "Currículo",
         conteudo: `
-            <h2>Arthur P. Nunes</h2>
-            <p>Desenvolvedor Full Stack</p>
-            <p>Experiência em JavaScript, Python, React e Node.js.</p>
-            <p>Formação: Ciência da Computação - Universidade XYZ.</p>
+            
         `
     },
     en: {
         titulo: "Resume",
         conteudo: `
-            <h2>Arthur P. Nunes</h2>
-            <p>Full Stack Developer</p>
-            <p>Experience with JavaScript, Python, React, and Node.js.</p>
-            <p>Education: Computer Science - XYZ University.</p>
-        `
+            <h1>Arthur P. Nunes</h1>
+            <span>Colonel Aparicio Borges Street, 976</span>
+            <p>
+            &emsp;I'm Arthur, a developer passionate about technology and
+            music. I've been in contact with programming since I was a teenager,
+            surfing the web and trying to understand a little bit of how everything is done.
+            </p>
+            <p>Life ended up taking me down other paths, where I ended up
+            serving the F.A.B, I started a technical course in Nursing, and soon after I tried to pursue a career in social media...</p>
+            <p> In 2022, I decided it was time to change and start doing what I was really interested in. So, I started a Full-Stack Web Developer course at Growdev, where I am learning and updating my knowledge for the future of the web. </p>
+            <h4><i>Find me on social media:</i></h4> `
     },
     es: {
         titulo: "Currículum",
@@ -46,6 +49,11 @@ function carregarConteudo() {
 // Função para voltar à página de seleção de idioma
 function voltar() {
     window.location.href = 'index.html';
+}
+
+function carregarCurriculo(idioma) {
+    localStorage.setItem('idioma', idioma); // Salva o idioma escolhido
+    window.location.href = 'sobremim'; // Redireciona para a página do currículo
 }
 
 // Carrega o conteúdo ao abrir a página do currículo
